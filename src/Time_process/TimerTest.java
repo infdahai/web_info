ffdc84c1341fff33f6834757a4662c78f7d1f88c
cluster <859287553@qq.com>
2019-01-07 13:06:56 +0800

    Update TimerTest.java

--- src/Time_process/TimerTest.java
+++ src/Time_process/TimerTest.java
@@ -1,30 +1,34 @@
 package Time_process;
 
+import doc_init.file_process;
+import doc_init.pickup_Topic;
+import doc_init.web_crawer;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
-import doc_init.*;
 
 public class TimerTest {
     Timer timer;
-    private static final long  period = 24*3600*1000;
+    private static final long period = 24 * 3600 * 1000;
+
     public TimerTest() {
         Date time = getTime();
         System.out.println("指定时间time=" + time);
         timer = new Timer();
-        //test_test  choose this way.  
-        // 1000 means 1s, 2000 means 2 second.  
+        //test_test  choose this way.
+        // 1000 means 1s, 2000 means 2 second.
         // 1000 is delay, 2000 is period
-        // timer.schedule(new TimerTaskTest(),1000,2000);
-        timer.schedule(new TimerTaskTest(),time,period);
+        timer.schedule(new TimerTaskTest(), 1000, 2000);
+        //timer.schedule(new TimerTaskTest(),time,period);
     }
 
     public Date getTime() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 12);
         calendar.set(Calendar.MINUTE, 00);
-        calendar.set(Calendar.SECOND,00);
+        calendar.set(Calendar.SECOND, 00);
         Date time = calendar.getTime();
         return time;
     }
@@ -40,8 +44,8 @@ public class TimerTest {
                 String[] args = {""};
                 Date date = new Date(this.scheduledExecutionTime());
                 System.out.println("本次执行该线程的时间为：" + date);
-                web_crawer.main(args,1);
-                file_process.main(args,0);
+                web_crawer.main(args, 1);
+                file_process.main(args, 0);
                 nlpCore.BasicPipelineExample.main(args);
                 pickup_Topic.main(args);
                 lucene.luceneIndex.main(args);
